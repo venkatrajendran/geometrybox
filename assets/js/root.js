a4352e3d548b78d2e5c2362cbc38e4ef7d560a90
Shanmuga priya D <somberbird@gmail.com>
2018-08-13 20:18:35 -0500

    Implemented changes suggested by Venkat.

--- assets/js/root.js
+++ assets/js/root.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $('.arrow').hide();
+    $('.prod').mouseenter(function(){
+        $(this).find('.arrow').show();
+    });
+    $('.prod').mouseleave(function(){
+        $(this).find('.arrow').hide();
+    });
     $('.pull-link').click(function(e){
         $('html, body').animate({
                     scrollTop: $("#desc").offset().top
